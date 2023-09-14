783237760e44cbcd3d5ff1b7c1bfc2223d25c168
yyjjvv <jiwonyoo66@gmail.com>
2023-09-14 23:21:32 +0900

    apply theme provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,17 @@
 import "@/styles/global.css";
+import { ThemeProvider } from "@/context/ThemeContext";
 
 import Header from "@/components/Layout/Header";
 import Container from "@/components/Layout/Container";
 
 const App = ({ Component, pageProps }) => {
     return (
-        <>
+        <ThemeProvider>
             <Header />
             <Container>
                 <Component {...pageProps} />
             </Container>
-        </>
+        </ThemeProvider>
     );
 };
 
